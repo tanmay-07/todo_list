94c55589ac39f36c0fee9ef8099d53413cf784dd
Tanmay sharma <tanmays95@gmail.com>
2017-02-28 19:11:00 +0530

    Add files via upload

--- app1.js
+++ app1.js
@@ -13,17 +13,45 @@
 // Get a reference to the database service
 var database = firebase.database();
 console.log(database);
-var dbref = database.ref();
+var dbref = database.ref().child("todos");
 
-dbref.on("value",function(e){
-	console.log(e.val());
-	var test = e.val();
-	console.log(test[0]);
-	for (var i = 0; i < test.length; i++) {
-		//test[i]
-		$("#list").append("<li><span class=\"content\">" + test[i]["name"] + "</span></li>");
-	}
+	/*	dbref.on("value",function(e){
+			console.log(e.val());
+			var test = e.val();
+			
+			
+
+		});
+    */
+   
+function loadData(){
+	dbref.on("child_added", function(snap){
+		console.log(snap.val());
+		var tst = snap.val();
+		var li  = document.createElement('li');
+		li.innerHTML = "<span class=\"content\"> "+ tst["name"] +"</span>  <button class=\"remove\">X</button>";
+		//li.className = "content";
+		li.id = snap.key;
+		//console.log(tst["task1"]);
+		$("#list").append(li);	
+		//changeClr();
+
+	});	
 	
+}
 
+loadData();
+
+dbref.on("child_changed", function(snap){
+	//console.log(snap);
+	const liChanged = document.getElementById(snap.key);
+	liChanged.innerText = snap.val()["name"];
+
+});
+
+dbref.on("child_removed", function(snap){
+	//console.log(snap);
+	const liToRemove = document.getElementById(snap.key);
+	liToRemove.remove();
 
 });
--- todo.js
+++ todo.js
@@ -1,40 +1,108 @@
+var database = firebase.database();
+console.log(database);
+var dbref = database.ref().child("todos");
+
+
+
 $(document).ready(function(){
-	
-	changeClr();
-		$(".content").on("click", function(){
-		$(this).toggleClass("strike");
-	});
-	$("#add").click(function(){
-		$("#default").toggle();
 
-	});
-	$("input").keypress(function (event) {
-		if(event.which == 13){
-			var inp ="";
-			inp = $(this).val();
-			$(this).val("");
-			$("#list").append("<li><span class=\"content\">" + inp + "</span></li>");
 
-			changeClr();
-		}
-		
-	})
+$("#add").on("click",function(){
+						$("#default").toggle();
 
+					});
+
+
+				var key1 = 10;
+					
+
+				$("input").keypress(function (event) {
+						if(event.which == 13){
+							var inp ="";
+							inp = $(this).val();
+							$(this).val("");
+							//$("#list").append("<li><span class=\"content\">" + inp + "</span></li>");
+							var newPostRef = dbref.push().set({
+				    			// ...
+				    			
+				    			"name":inp,
+								"comlpleted": false,
+								"id": key1		
+
+							});
+				}
+				});		
+				var lis = document.querySelectorAll("li");
+				for (var i = 0; i < lis.length; i++) {
+							
+							//var idd = this;
+							//console.log(idd);
+							$("#container").on('mouseenter', 'li', function() {
+    								//do something
+    								$(this).find("button").addClass("vis_rem");
+							});
+							$("#container").on('mouseleave', 'li', function() {
+    								//do something
+    								$(this).find("button").removeClass("vis_rem");
+							});
+
+								/*
+
+						$(lis[i]).hover(function(){
+							$(this).find("button").toggle(".remove");
+							
+						});   
+							*/
+				}
+
+				$(document).on("click", '.remove', function(){
+						console.log(this);
+						$(this).parent().remove();
+						var keyToBeDeleted = $(this).parent().get(0).id;
+						var delRef = dbref.child(keyToBeDeleted);
+						delRef.remove();
+						console.log(keyToBeDeleted);
+				});				
+				
 
 
 });
-function changeClr(argument) {
-	// body...
-	$("li").each(function(){
-		console.log("hii");
-		var index = $(this).index();
-		if(index%2==0){
-			$(this).css("background", "#E3E3E3");
-		}
-		$(this).hover(function(){
-			console.log("hovered");
-			$("#remove").toggle();
-		});
-	});
 
-}
+setTimeout( function(){
+
+
+	
+
+}, 2000);
+						
+
+
+
+
+	//changeClr();
+/*
+		$(".content").on("click", function(){
+		$(this).toggleClass("strike");
+
+		var keyToBeCompleted = $(this).parent().get(0).id;
+		console.log(keyToBeCompleted);
+
+		
+		var taskref = dbref.child(keyToBeCompleted);
+		taskref.once('value', function(snapshot) {
+
+    		if( snapshot.val() === null ) {
+        		/* does not exist 
+    		} else {
+        		console.log(snapshot.val());
+        		taskref.update({"comlpleted": true});
+    		}
+
+});     
+
+
+		dbref.orderByChild('comlpleted').on('value',function(snap){
+			//console.log(snap.val());
+		})
+		
+	});    */
